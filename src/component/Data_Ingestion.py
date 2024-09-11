44a301daf7de276d2882b0236cf576bac117e194
ankitgaur0 <ankitparashar000@gmail.com>
2024-09-11 16:41:48 +0530

    Data_Ingestion is updated with new code

--- src/component/Data_Ingestion.py
+++ src/component/Data_Ingestion.py
@@ -1,66 +1,70 @@
 import os,sys
 from pathlib import Path
-import cv2
+import random
 import pandas as pd
 import numpy as np
+import matplotlib.pyplot as plt
 from src.Logger import logging
 from src.Exception_Handler import Custom_Exception
 from src.Utils import resized_image
 from src.Utils import save_obj
 from dataclasses import dataclass
+import tensorflow
+from tensorflow.keras.preprocessing.image import ImageDataGenerator
 
 @dataclass
 class Data_Ingestion_Config:
-    features_data_path :str =os.path.join("artifacts","X.pickle")
-    label_data_path :str =os.path.join("artifacts","y.pickle")
+    images_data_path :str =os.path.join("artifacts","images_data.pickle")
+    label_data_path :str =os.path.join("artifacts","labels_data.pickle")
 
 class Data_Ingestion:
-    def __init__(self,data_dir,image_size=(50,50)):
+    def __init__(self,data_dir):
         self.Data_Ingestion_Config_obj=Data_Ingestion_Config()
         self.data_dir=data_dir
-        self.image_size=image_size
-        self.data=[]
-        self.label=[]
+
+
+
+    def initiate_augmentation(self,rescale=1.0/255,rotation_range=40,width_shift_range=0.2,height_shift_range=0.2,shear_range=0.2,zoom_range=0.2,horizontal_flip=True,fill_mode="nearest"):
+        train_aug_data_gen=ImageDataGenerator(
+            rescale=rescale,
+            rotation_range=rotation_range,
+            width_shift_range=width_shift_range,
+            height_shift_range=height_shift_range,
+            shear_range=shear_range,
+            zoom_range=zoom_range,
+            horizontal_flip=horizontal_flip,
+            fill_mode=fill_mode
+        )
+
+        return train_aug_data_gen
+    
 
 
     def initiate_data(self):
         try:
-            logging.info("initiating the data is starting")
-            #data_dir=Path("/home/ankit/dogs_vs_cats_project/notebook/Data/animal")
-            CATEGORIES=["cat","dog"]
+       
+            images_path=self.data_dir
 
-            for category in CATEGORIES:
-                dir_path=os.path.join(self.data_dir,category)
-                class_label=CATEGORIES.index(category) # 0 for cat and 1 for cat
-
-                for image_name in os.listdir(dir_path):
-                    img_path=os.path.join(dir_path,image_name) #image_name provide the individuals names of the images
-                    image=cv2.imread(img_path)
-                    if image is None:
-                        continue #if the image is currpted then skip and continue
-
-                    resized_img=resized_image(image,self.image_size)
-                    self.data.append(resized_img)
-                    self.label.append(class_label)
-
-            data_array=np.array([self.data])
-            label_array=np.array([self.label])
-
-            save_obj(
-                self.Data_Ingestion_Config_obj.features_data_path,data_array
-            )
-            save_obj(self.Data_Ingestion_Config_obj.label_data_path,label_array)
-
-
-            return(
-                data_array,
-                label_array
+            aug_data=self.initiate_augmentation(rescale=1.0/255,rotation_range=40,width_shift_range=0.2,height_shift_range=0.2,shear_range=0.2,zoom_range=0.2,horizontal_flip=True,fill_mode="nearest")
+            train_data=aug_data.flow_from_directory(
+                images_path,
+                target_size=(50,50),
+                batch_size=32,
+                class_mode='binary'
             )
 
+            images,labels=next(train_data)
+            logging.info(f"this is the shape of the input features {images.shape}")
+            logging.info(f"the shape of the lables list is {labels.shape}")
 
+            #now load the first images in the log
+            logging.info(f"the first images is :\n label is {labels[0]} \n {plt.imshow(images[0]),plt.show()} ")
 
+            
+            save_obj(self.Data_Ingestion_Config_obj.images_data_path,obj=images)
+            save_obj(self.Data_Ingestion_Config_obj.label_data_path,obj=labels)
 
-
-
+            return train_data
         except Exception as e:
             raise Custom_Exception(e,sys)
+       
