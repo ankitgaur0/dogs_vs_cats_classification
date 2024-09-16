3d5338e7b98450bdb49f4a40993f6ba5ead26449
ankitgaur0 <ankitparashar000@gmail.com>
2024-09-16 16:22:24 +0530

    Data_Ingestion is completed

--- src/component/Data_Ingestion.py
+++ src/component/Data_Ingestion.py
@@ -1,70 +1,81 @@
 import os,sys
 from pathlib import Path
-import random
+
 import pandas as pd
 import numpy as np
-import matplotlib.pyplot as plt
+import cv2
+from dataclasses import dataclass
 from src.Logger import logging
 from src.Exception_Handler import Custom_Exception
-from src.Utils import resized_image
-from src.Utils import save_obj
-from dataclasses import dataclass
-import tensorflow
-from tensorflow.keras.preprocessing.image import ImageDataGenerator
+
+#for spliting the data use sklearn 
+from sklearn.model_selection import train_test_split
+
 
 @dataclass
-class Data_Ingestion_Config:
-    images_data_path :str =os.path.join("artifacts","images_data.pickle")
-    label_data_path :str =os.path.join("artifacts","labels_data.pickle")
+class Data_Config:
+    raw_data_path :str =os.path.join("artifacts","raw_data.csv")
+    train_data_path :str =os.path.join("artifacts","train.csv")
+    test_data_path :str =os.path.join("artifacts","test.csv")
+
 
 class Data_Ingestion:
-    def __init__(self,data_dir):
-        self.Data_Ingestion_Config_obj=Data_Ingestion_Config()
-        self.data_dir=data_dir
-
-
-
-    def initiate_augmentation(self,rescale=1.0/255,rotation_range=40,width_shift_range=0.2,height_shift_range=0.2,shear_range=0.2,zoom_range=0.2,horizontal_flip=True,fill_mode="nearest"):
-        train_aug_data_gen=ImageDataGenerator(
-            rescale=rescale,
-            rotation_range=rotation_range,
-            width_shift_range=width_shift_range,
-            height_shift_range=height_shift_range,
-            shear_range=shear_range,
-            zoom_range=zoom_range,
-            horizontal_flip=horizontal_flip,
-            fill_mode=fill_mode
-        )
-
-        return train_aug_data_gen
-    
-
+    def __init__(self,data_path) -> None:
+        self.data_config_obj=Data_Config()
+        self.data_path=data_path
+        self.image_size=70
 
     def initiate_data(self):
         try:
-       
-            images_path=self.data_dir
+            images_path=self.data_path
 
-            aug_data=self.initiate_augmentation(rescale=1.0/255,rotation_range=40,width_shift_range=0.2,height_shift_range=0.2,shear_range=0.2,zoom_range=0.2,horizontal_flip=True,fill_mode="nearest")
-            train_data=aug_data.flow_from_directory(
-                images_path,
-                target_size=(50,50),
-                batch_size=32,
-                class_mode='binary'
+            categories=["cat","dog"]
+            #storing the data and labels in the list
+            data=[]
+            labels=[]
+
+            for category in categories:
+                path=os.path.join(images_path,category)
+                image_label=categories.index(category)
+
+                for image_name in os.listdir(path):
+                    full_image_path=os.path.join(path,image_name)
+                    img=cv2.imread(full_image_path)
+
+                    if img is not None:
+                        new_img=cv2.resize(img,(self.image_size,self.image_size)) #resize the images of 70*70
+                        #store the data into data list &store the label in the label list
+                        data.append(new_img)
+                        data.append(image_label)
+            #converting the list to array
+            data=np.array(data)
+            labels=np.array(labels)
+
+            #now concate the data and labels so that can do spliting
+            full_data_array=np.c_[data,labels]
+
+            #store the orinial datat to artifacts
+            row_artifacts_path=os.path.join(os.path.dirname(self.data_config_obj.raw_data_path))
+            #now make the artifacts folder
+            os.makedirs(row_artifacts_path,exist_ok=True)
+            
+            full_data_array.to_csv(self.data_config_obj.raw_data_path,index=False)
+            logging.info("store the raw data successfully")
+
+            #now spliting the array into trian and test data
+            train_data,test_data=train_test_split(full_data_array,test_data=0.20,random_state=42)
+
+            # store the train and test data in artifacts
+            train_data.to_csv(self.data_config_obj.train_data_path,index=False)
+            logging.info("store the train_data in the artifacts completed")
+            test_data.to_csv(self.data_config_obj.test_data_path,index=False)
+            logging.info("storing the test_data to artifacts is compeleted")
+
+            return(
+                train_data,
+                test_data
             )
 
-            images,labels=next(train_data)
-            logging.info(f"this is the shape of the input features {images.shape}")
-            logging.info(f"the shape of the lables list is {labels.shape}")
 
-            #now load the first images in the log
-            logging.info(f"the first images is :\n label is {labels[0]} \n {plt.imshow(images[0]),plt.show()} ")
-
-            
-            save_obj(self.Data_Ingestion_Config_obj.images_data_path,obj=images)
-            save_obj(self.Data_Ingestion_Config_obj.label_data_path,obj=labels)
-
-            return train_data
         except Exception as e:
             raise Custom_Exception(e,sys)
-       
